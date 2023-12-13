67c3cca019d98d5cd29a05c87410fdbbb4c41184
ShivanG <shivangramola007@gmail.com>
2023-12-13 19:04:32 +0530

    new updates by shivang

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+import stationList from "./StationList.json";
+
 const App = () => {
+  const [start, setStart] = useState("");
+  const [end, setEnd] = useState("");
+
+  let station = stationList.stations;
+
   async function getData(start, end) {
     const url = `https://us-central1-delhimetroapi.cloudfunctions.net/route-get?from=${start}&to=${end}`;
     const res = await fetch(url);
@@ -6,7 +14,20 @@ const App = () => {
     console.log(data);
   }
 
-  getData();
+  getData(start, end);
+
+  const handleClick = () => {
+    console.log("starting");
+  };
+
+  const handleChange = (val) => {
+    console.log(val);
+    setStart(val);
+  };
+  const handleChange2 = (val) => {
+    console.log(val);
+    setEnd(val);
+  };
 
   return (
     <div>
@@ -15,21 +36,51 @@ const App = () => {
         <div className="main-content flex flex-wrap gap-36">
           <div className="from">
             <h2 className="text-3xl mb-2">journey start station</h2>
-            <select>
-              <option value="1">1</option>
+            <select
+              className="border-2 border-gray-700 p-1 rounded-lg"
+              onChange={(e) => handleChange(e.target.value)}
+            >
+              {station.map((ele, i) => {
+                if (i === 0)
+                  return (
+                    <option key={i} value={ele} hidden>
+                      {ele}
+                    </option>
+                  );
+                return (
+                  <option key={i} value={ele}>
+                    {ele}
+                  </option>
+                );
+              })}
             </select>
           </div>
           <div className="to">
             <h2 className="text-3xl mb-2">journey end station</h2>
-            <select>
-              <option value="1">1</option>
+            <select
+              onChange={(e) => handleChange2(e.target.value)}
+              className="border-2 border-gray-700 p-1 rounded-lg"
+            >
+              {station.map((ele, i) => {
+                if (i === 0)
+                  return (
+                    <option key={i} value={ele} hidden>
+                      {ele}
+                    </option>
+                  );
+                return (
+                  <option key={i} value={ele}>
+                    {ele}
+                  </option>
+                );
+              })}
             </select>
           </div>
         </div>
       </div>
       <div className="flex justify-center mt-5">
         <button
-          // onClick={handleClick}
+          onClick={handleClick}
           className="bg-orange-600 text-white rounded-lg p-2"
         >
           search for route
